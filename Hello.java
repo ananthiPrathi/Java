840e58bc655ad8b0b80f75dd23aaac77ce5742ae
ananthiPrathi <ananthi2001k@gmail.com>
2025-01-31 14:12:28 -0800

    oneline are inserted

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
+
 Write the java program in print Hello
   public class Hello{
   public static void main(String[] args)
   {
     System.out.println("Hello java");
+    System.out.println("Hello java World");
   }
 }
 

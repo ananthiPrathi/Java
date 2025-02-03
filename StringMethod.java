560312d4f1fd215d4725756b18583bbbdc531580
ananthiPrathi <ananthi2001k@gmail.com>
2025-02-03 13:10:49 -0800

    corrected code

--- StringMethod.java
+++ StringMethod.java
@@ -4,21 +4,21 @@ public static void main(String[] args)
  String    a = "     Everthing in the World";
  String  b = "welcome";
  String c = "World";
- System.out.println(a.charAt(0); 
-
+ System.out.println(a.charAt(0)); 
+ System.out.println(a.trim());
  System.out.println(a.indexOf("i"));
  System.out.println(a.lastIndexOf("r"));
  System.out.println(a.toUpperCase());
  System.out.println(a.toLowerCase());
- System.out.println(a.trim(" "));
+ 
  System.out.println(a.split(" "));
  System.out.println(b.concat(c));
  System.out.println(c.length());
  System.out.println(a.substring(11,13));
- System.out.println(a.equlals(b));
- System.out.println(a.replac3('i','f');
+ System.out.println(a.equals(b));
+ System.out.println(a.replace('i','f'));
  System.out.println(a.startsWith("Eve"));
- System.out.println(b.endsWith('d'));
+ System.out.println(b.endsWith("e"));
  
 }
 }

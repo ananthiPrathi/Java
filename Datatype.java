be322012cbcb07270c2e1663e11325878032078f
ananthiPrathi <ananthi2001k@gmail.com>
2025-02-03 11:34:53 -0800

    Datatype notes are added

---
+++ Datatype.java
@@ -0,0 +1,9 @@
+Data type is a classification of specifie the value of variable can hold on the 
+operation can peroform on it.
+There two types of datatype
+1.primitive - only single value stored(byte,short,int,long,double,float,char,boolean)
+2.Non-primitive - reference data type(class,object,string,array,interface)
+
+
+ 
+
--- Scanner.java
+++
@@ -1,2 +0,0 @@
-scanner class in java
-

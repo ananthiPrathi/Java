bc303e355d963d5bf55c2adf06095571473ea1e5
ananthiPrathi <ananthi2001k@gmail.com>
2025-02-03 12:36:09 -0800

    semicolon will be included

--- Stringbasic.java
+++ Stringbasic.java
@@ -2,7 +2,8 @@ public class Stringbasic{
 public static void main(String[] args)
 {
  String a =  "Hello java World";
-String obj = new String("Hello java World");
-Systm.out.println(a)
+String obj = new String("Hello java World!");
+System.out.println(a);
+System.out.println(obj);
 }
 }

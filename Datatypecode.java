1f12a5de418d3231a3883b2761e8b8fed400267d
Ananthi <118195006+ananthiPrathi@users.noreply.github.com>
2025-02-02 22:16:27 -0800

    Update Datatypecode.java

--- Datatypecode.java
+++ Datatypecode.java
@@ -2,13 +2,24 @@ public class Datatype
 {
 public static void main(String[] args)
 {
-  int i = 100;
+  int i = 100;    
   byte b = 12;
+  short s = 123;
+  long l = 9875433;
   double d =9.0000;
   float f = 43.9876f;
+  boolean b = true;
+  char c = 'a';
    System.out.println(i);
    System.out.println(b);
    System.out.println(d);
    System.out.println(f);
+    System.out.println(s);
+    System.out.println(l);
+    System.out.println(b);
+    System.out.println(c);
+  String name = "Ananthi";
+  System.out.println(name);
+  
 }
 }

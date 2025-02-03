4f6f030965d5c092ecfa8eb1b894a6a262c6aae6
ananthiPrathi <ananthi2001k@gmail.com>
2025-02-03 11:39:50 -0800

    Datatypecode are inserted

---
+++ Datatypecode.java
@@ -0,0 +1,14 @@
+public class Datatype
+{
+public static void main(String[] args)
+{
+  int i = 100;
+  byte b = 12;
+  double d =9.0000;
+  float f = 43.9876f;
+   System.out.println(i);
+   System.out.println(b);
+   System.out.println(d);
+   System.out.println(f);
+}
+}

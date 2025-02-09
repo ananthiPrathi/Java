e3d654443d6984718dca48230e6a2432e9eaa421
ananthiPrathi <ananthi2001k@gmail.com>
2025-02-09 11:50:32 -0800

    square fill Pattern

---
+++ Pattern.java/pattern1.java
@@ -0,0 +1,12 @@
+public class Pattern1{//Square fill pattern
+ public static void main(String[] args)
+{
+  for (int i=0;i<=6;i++)
+{
+   for(int j=0;j<=6;j++)
+{
+     System.out.println("*" +" ");
+}
+      System.out.println();
+}
+}

55fcd8ff46bdbffd1298687e77943fc293a6359c
ananthiPrathi <ananthi2001k@gmail.com>
2025-02-15 16:33:35 -0800

    Collections notes are added

---
+++ Collections.java
@@ -0,0 +1,23 @@
+Collections is a FrameWork the Collection 
+in Java is a framework that provides an architecture 
+to store and manipulate the group of objects.
+
+Java Collections can achieve all the operations
+that you perform on a data such as searching, 
+sorting, insertion, manipulation, and deletion.
+
+Java Collection means a single unit of objects. 
+Java Collection framework provides many 
+interfaces (Set, List, Queue, Deque) and
+classes (ArrayList, Vector, LinkedList, PriorityQueue, HashSet, LinkedHashSet, TreeSet).
+
+A framework provides a ready-made structure of 
+classes and interfaces for building software applications 
+efficiently.
+
+The Collection framework represents a unified architecture for storing and manipulating a group of objects. 
+It enhances code efficiency and readability by offering various data structures, including 
+arrays, linked lists, trees, and hash tables, tailored to different programming needs. 
+It has:
+Interfaces and its implementations, i.e., classes
+Algorithm
